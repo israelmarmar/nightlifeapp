ee0c67f0ee4eaba6c43626f8b8869e4549f7a458
israel marques martins <emaill.israel.br@gmail.com>
2017-09-06 10:21:02 -0400

    change files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var _requestSecret;
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var router = express.Router();
+
 var session=require('express-session');
 var cookieParser = require('cookie-parser');
 var request = require("request");
@@ -21,24 +21,33 @@ var YELP_ID="nM3rD0hDEaAPvGZt9CsHgQ";
 var YELP_CLIENT="d5xql3xJuP3UisVzcNDuAkw3BLha3xHGBmMaP81XM54RT59I3izc52pcA302DmXe";
 var urldb =process.env.MONGOLAB_URI || 'mongodb://urlshort:78292725@ds127993.mlab.com:27993/israelmarmar';     
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+var db;
 
-router.use(cookieParser());
-router.use(session({secret: 'some secret key',resave: "", // add this; choose the value you want from the docs
+app.use(cookieParser());
+app.use(session({secret: 'some secret key',resave: "", // add this; choose the value you want from the docs
   saveUninitialized: "" // add this; choose the value you want from the docs
 				}));
 
-router.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
-router.get('/logout', function (req, res) {
+MongoClient.connect(urldb, function(err, database) {
+  if(err) throw err;
+
+  db = database;
+
+
+});
+
+app.get('/logout', function (req, res) {
 	req.session.destroy();
 	res.redirect("https://votingapp-isrmm.herokuapp.com");
 });
 
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile("/main.html",{root: __dirname});
 });
 
-router.get('/apijson', function (req, res) {
+app.get('/apijson', function (req, res) {
 var loc=req.query.loc;
 var token;
 var config;
@@ -79,6 +88,12 @@ var config;
 
   */
   
+  function countvl(obj,id){
+	  return obj.filter(function(v) {
+  return v.id=id;
+}).length;
+  }
+  
   var options = { method: 'POST',
   "rejectUnauthorized": false, 
   url: 'https://api.yelp.com/oauth2/token',
@@ -100,8 +115,25 @@ var config;
 	request(options, function (error, response, body) {
 		if (error) throw new Error(error);
 console.log(loc);
-		res.setHeader('Content-Type', 'application/json'); 
-		res.send(body);
+		var json=JSON.parse(body).businesses;
+		
+		var array=[];
+		for(var i=0;i<json.length;i++){
+		array.push(json[i].id);	
+		}
+		
+		db.collection("nightlife").find( { idloc: { $in: array } }).toArray(function(err, result) {
+			if (err) throw err;
+			
+			for(var i=0;i<json.length;i++){
+				
+			json[i].going=countvl(result,json[i].id);	
+			}
+			
+		res.json(json);
+	
+		});
+		
 		});
 		
 
@@ -111,7 +143,38 @@ console.log(loc);
 
 });
 
-router.get("/request-token", function(req, res) {
+app.get("/going", function(req, res) {
+      var id=req.query.id;
+		if(req.session.user){
+			
+			db.collection("nightlife").find( { user: JSON.parse(req.session.user).screen_name}).toArray(function(err, result) {
+			if (err) throw err;
+			
+				if(!(result)){
+				db.collection("nightlife").insertOne({user:JSON.parse(req.session.user).screen_name,idloc:id}, function(err, res) {
+				if (err) throw err;
+					db.collection("nightlife").count().then({idloc:id},function(value){
+						res.json({msg:value});
+					});
+				
+				});
+				}else{
+				db.collection("nightlife").deleteOne({user:JSON.parse(req.session.user).screen_name,idloc:id}, function(err, obj) {
+				if (err) throw err;
+					db.collection("nightlife").count().then({idloc:id},function(value){
+					res.json({msg:value});
+					});
+				});
+				}
+			
+			});
+			
+		}else{
+			res.json({msg:"User is not signed"});
+		}
+});
+
+app.get("/request-token", function(req, res) {
         twitter.getRequestToken(function(err, requestToken, requestSecret) {
             if (err)
                 res.status(500).send(err);
@@ -123,7 +186,7 @@ router.get("/request-token", function(req, res) {
     });
 	
   
- router.get("/access-token", function(req, res) {
+ app.get("/access-token", function(req, res) {
         var requestToken = req.query.oauth_token,
       verifier = req.query.oauth_verifier;
 
@@ -142,10 +205,11 @@ router.get("/request-token", function(req, res) {
         });
     });
 
+	
+	app.get("/going", function(req, res) {
+
+	 });
 
 app.listen(port, function () {
  console.log("ligado");
 });
-
-
-app.use('/', router);
